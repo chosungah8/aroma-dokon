ee205c0cb16c41ad5af7934d25bd82454bbb639a
chosungah8/aroma-dokon <chosungah8@gmail.com>
2026-09-09 23:01:40 +0900

    fix api test

--- api/test.js
+++ api/test.js
@@ -1,14 +1,7 @@
 export default function handler(request) {
-    return new Response(
-        JSON.stringify({
-            success: true,
-            message: "Vercel API ishlayapti"
-        }),
-        {
-            status: 200,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-    );
+    return Response.json({
+        success: true,
+        message: "Aro'ma API ishlayapti"
+    });
 }
+
