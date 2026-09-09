8fe3166671c9834d35593f1622b37230006dcc8e
chosungah8/aroma-dokon <chosungah8@gmail.com>
2026-09-09 22:03:58 +0900

    fix vercel api test

--- api/test.js
+++ api/test.js
@@ -1,7 +1,6 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
     res.status(200).json({
         success: true,
         message: "Vercel API ishlayapti"
     });
-}
-
+};
